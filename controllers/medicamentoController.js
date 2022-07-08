d4d0f73173b0c031866a91b2bb0c530db5daa0a7
Adrian Moreno <adrianemoreno.25@gmail.com>
2022-07-07 20:52:58 -0400

    Terminando medicamentos con validacion

--- controllers/medicamentoController.js
+++ controllers/medicamentoController.js
@@ -17,13 +17,10 @@ module.exports = {
 		try {
 			await db.Medicamento.create({
 				data: {
-					ci: req.body.cedula,
-					nombre: req.body.nombre,
-					apellido: req.body.apellido,
-					edad: parseInt(req.body.edad),
-					telefono: req.body.telefono,
-					id_cargo: parseInt(req.body.id_cargo),
-					id_farmacia: parseInt(req.body.id_farmacia),
+					monodroga: req.body.monodroga,
+					presentacion: req.body.presentacion,
+					accion: req.body.accion,
+					precio: parseFloat(req.body.precio),
 				}
 			});
 		} catch (e) {
@@ -34,49 +31,34 @@ module.exports = {
 		return res.redirect('/medicamento?&creado=1');
 	},
 	edit: async (req, res) => {
-		const medicamentos = await db.Medicamento.findMany({
-			include: {
-				cargo: true,
-			}
-		});
-		const e = await db.Medicamento.findUnique({
+		const medicamentos = await db.Medicamento.findMany();
+		const m = await db.Medicamento.findUnique({
 			where: {
 				id: parseInt(req.params.id),
 			},
-			include: {
-				cargo: true,
-				farmacia: true,
-			}
 		});
-		const farmacias = await db.Farmacia.findMany();
-		const cargos = await db.Cargo.findMany();
 		return res.render('medicamento/index', {
+
 			medicamentos: medicamentos,
-			farmacias: farmacias,
-			cargos: cargos,
 			titulo: "Medicamentos",
 			creado: req.query.creado,
 			editado: req.query.editado,
 			eliminado: req.query.eliminado,
-			e: e,
+			m: m,
 			edit: true
 		});	
 	},
 	update: async (req,res) => {
-
 		try {
 			await db.Medicamento.update({
 				where: {
 					id: parseInt(req.params.id),
 				},
 				data: {
-					ci: req.body.cedula,
-					nombre: req.body.nombre,
-					apellido: req.body.apellido,
-					edad: parseInt(req.body.edad),
-					telefono: req.body.telefono,
-					id_cargo: parseInt(req.body.id_cargo),
-					id_farmacia: parseInt(req.body.id_farmacia),
+					monodroga: req.body.monodroga,
+					presentacion: req.body.presentacion,
+					accion: req.body.accion,
+					precio: parseFloat(req.body.precio),
 				}
 			});
 		} catch (e) {
